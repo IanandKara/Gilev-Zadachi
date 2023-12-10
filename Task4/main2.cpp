425a973a03748bbc4f0a1942fe9cb53802f9a07e
IanandKara <145831940+IanandKara@users.noreply.github.com>
2023-12-10 15:18:11 +0200

    Задача 4_2

---
+++ Task4/main2.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+
+int main()
+{
+    int N;
+    std::cin >> N;
+
+    switch(N)
+    {
+
+    case 1:
+        std::cout << "It's January";
+        break;
+    case 2:
+        std::cout << "It's February";
+        break;
+    case 3:
+        std::cout << "It's March";
+        break;
+    case 4:
+        std::cout << "It's April";
+        break;
+    case 5:
+        std::cout << "It's May";
+        break;
+    case 6:
+        std::cout << "It's June";
+        break;
+    case 7:
+        std::cout << "It's July";
+        break;
+    case 8:
+        std::cout << "It's August";
+        break;
+    case 9:
+        std::cout << "It's September";
+        break;
+    case 10:
+        std::cout << "It's October";
+        break;
+    case 11:
+        std::cout << "It's November";
+        break;
+    case 12:
+        std::cout << "It's December";
+        break;
+
+    default:
+        std::cout << "Wrong month number";
+
+    }
+}
