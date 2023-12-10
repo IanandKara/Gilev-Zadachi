93915ec774acc56e9a5e3b331140b5d385571911
IanandKara <145831940+IanandKara@users.noreply.github.com>
2023-12-10 15:17:39 +0200

    Задача 4_1

---
+++ Task4/main1.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+int main()
+{
+    int A, B, C;
+    std::cin >> A >> B >> C;
+    
+    if (A % B == 0)
+    {
+        if (B > C) {
+            std::cout << A / B + C;
+        }
+        else {
+            std::cout << A / B - C;
+        }
+    }
+    else {
+        std::cout << (A + B) * C;
+    }
+}
