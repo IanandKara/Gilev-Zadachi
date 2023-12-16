ffad5a2d3224147e7e72dd353f6253eee0e1f25b
IanandKara <145831940+IanandKara@users.noreply.github.com>
2023-12-16 03:49:03 +0200

    Задача 8_2

---
+++ Task8/main2.cpp
@@ -0,0 +1,62 @@
+#include <iostream>
+#include <fstream>
+#include "functions.hpp"
+
+int main()
+{
+    /*
+    Дана строка, содержащая русский текст. Если в тексте нет слов-палиндромов длиной более 1-й буквы, 
+    то вывести слова текста в соответствии с убыванием количества согласных, в противном    
+    случае пpодублиpовать в словах текста гласные буквы и вывести полученные слова в поpядке, обpатном к алфавитному.
+    */
+
+    setlocale(LC_ALL, "Rus");
+
+    std::ifstream in("input.txt");
+    char text[1000];
+    in.getline(text, 1000);
+
+    // Разделение текста
+    char words[500][50];
+    int n = 0;
+    char tmp[50];
+    bool flag = false;
+    ag::SplitText(text, words, n);
+
+    // Условия и действия
+    for (int i = 0; i < n; i++)
+        if (ag::IsPalindrome(words[i]) && (strlen(words[i] > 1))
+            flag = true;
+
+
+    if (flag == 1)
+    {
+        for (int j = 0; j < n - 1; j++)  
+            for (int k = j + 1; k < n; k++)
+                if (tolower(words[j][0]) < tolower(words[k][0]))
+                {
+                    strcpy_s(tmp, words[j]);
+                    strcpy_s(words[j], words[k]);
+                    strcpy_s(words[k], tmp);   
+                }
+        for (int j = 0; j < n; j++)
+            ag::duplicateVowels(words[j]);
+    }
+    else
+    {
+        for(int j = 0; j < n - 1; j++)
+            for(int k = j + 1; k < n; k++)
+                if (ag::numOfConsonant(words[j]) < ag::numOfConsonant(words[k]))  
+                {
+                    strcpy_s(tmp, words[j]);
+                    strcpy_s(words[j], words[k]);
+                    strcpy_s(words[k], tmp);   
+                }
+    }
+    
+    
+
+
+    for (int i = 0; i < n; i++)
+        std::cout << "<" << words[i] << "> " << std::endl;
+}
