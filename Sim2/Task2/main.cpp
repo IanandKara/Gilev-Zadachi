e8e41797c8c7c357bc1c2216f2c6ce3f5a2a8629
IanandKara <145831940+IanandKara@users.noreply.github.com>
2024-05-04 02:23:29 +0200

    Полная сортировка

---
+++ Sim2/Task2/main.cpp
@@ -0,0 +1,68 @@
+﻿#include <iostream>
+#include <cmath>
+
+class Rectangle
+{
+    // Поля
+    int Len, Wid;
+
+public:
+    // Конструкторы
+    Rectangle() = default;
+    Rectangle(int a, int b)
+    {
+        SetLW(a, b);
+    }
+
+    void SetLW(int a, int b) // Сеттер
+    {
+        if ((a < 0) || (b < 0))
+        {
+            std::cout << "Error: negative length" << std::endl;
+        }
+        Len = a;
+        Wid = b;
+    }
+
+    int Area() // Площадь
+    {
+        return Len * Wid;
+    }
+
+    int Perimeter() // Периметр
+    {
+        return 2 * (Len + Wid);
+    }
+
+    double Diagonal() // Диагональ
+    {
+        return sqrt(Len * Len + Wid * Wid);
+    }
+
+    void Print() // Вывод значений
+    {
+        std::cout << "Area = " << Area() << std::endl;
+        std::cout << "Perimeter = " << Perimeter() << std::endl;
+        std::cout << "Diagonal = " << Diagonal() << std::endl;
+    }
+};
+
+
+int main()
+{
+    Rectangle RE1{};
+    int a, b;
+    std::cout << "Write Rectangle_1 length and width: ";
+    std::cin >> a >> b;
+    RE1.SetLW(a, b);
+
+    std::cout << "Rectangle_1:" << std::endl;
+    RE1.Print();
+
+
+    Rectangle RE2(4, 3);
+    std::cout << "\n" <<"Rectangle_2:" << std::endl;
+    RE2.Print();
+
+    return 0;
+}
